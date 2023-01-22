a74cc8b6e6c1ce6227b9f30e1e080d2de1f2b031
howard127 <kobun127@gmail.com>
2023-01-22 18:29:04 +0900

    [add] エンパを追加

---
+++ src/main/java/com/github/elic0de/thejpspit/spigot/item/items/ItemEnderPearl.java
@@ -0,0 +1,38 @@
+package com.github.elic0de.thejpspit.spigot.item.items;
+
+import com.github.elic0de.thejpspit.spigot.item.PitItemEntry;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemEnderPearl extends PitItemEntry {
+
+    @Override
+    public String getId() {
+        return "ender_pearl";
+    }
+
+    @Override
+    public ItemStack getRawItemStack() {
+        return new ItemStack(Material.ENDER_PEARL);
+    }
+
+    @Override
+    public int getPrice() {
+        return 100;
+    }
+
+    @Override
+    public int getRequiredLevel() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "§aエンダーパール";
+    }
+
+    @Override
+    public char getSlotChar() {
+        return 'e';
+    }
+}
