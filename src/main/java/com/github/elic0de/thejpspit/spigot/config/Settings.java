a104b2b4bfd4cd3524f2ea84df83493c906c9344
oieii <lpondboy@gmail.com>
2023-01-22 16:10:56 +0900

    アクセストークンの警告コメントついかー

--- src/main/java/com/github/elic0de/thejpspit/spigot/config/Settings.java
+++ src/main/java/com/github/elic0de/thejpspit/spigot/config/Settings.java
@@ -1,5 +1,6 @@
 package com.github.elic0de.thejpspit.spigot.config;
 
+import net.william278.annotaml.YamlComment;
 import net.william278.annotaml.YamlFile;
 import net.william278.annotaml.YamlKey;
 
@@ -7,6 +8,7 @@ import net.william278.annotaml.YamlKey;
 public class Settings {
 
     @YamlKey("github_token")
+    @YamlComment("アクセストークンが漏洩すると、被害が広範囲に及ぶことになりますので公開しないでください")
     private String githubToken = "";
 
     public String getGithubToken() {
