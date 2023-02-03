2291b03da839d7eb651db7fb4dcae0c970ba442b
oieii <lpondboy@gmail.com>
2023-02-03 15:40:50 +0900

    tne対応

--- src/main/java/com/github/elic0de/thejpspit/cosmetics/CosmeticManager.java
+++ src/main/java/com/github/elic0de/thejpspit/cosmetics/CosmeticManager.java
@@ -24,7 +24,6 @@ public class CosmeticManager {
     ClassToInstanceMap<TrailCosmetic> trailCosmetics;
     ClassToInstanceMap<AuraCosmetic> auraCosmetics;
 
-
     public ClassToInstanceMap<AbstractCosmetic> allCosmetics;
 
     public CosmeticManager() {
