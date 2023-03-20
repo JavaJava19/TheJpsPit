c1c14494cf9e67f6624802495e45a308bea6d990
oieii <lpondboy@gmail.com>
2023-03-20 09:01:14 +0900

    feat サバイバルモードでも壊せないようにするやつ

--- src/main/java/com/github/elic0de/thejpspit/listener/BlockPlaceListener.java
+++ src/main/java/com/github/elic0de/thejpspit/listener/BlockPlaceListener.java
@@ -30,6 +30,9 @@ public class BlockPlaceListener implements Listener {
         if (player.getGameMode() == GameMode.CREATIVE) return;
         final BlockState replacedState = event.getBlockReplacedState();
 
+        // クリエイティブ以外は壊せないように
+        event.setCancelled(true);
+
         if (replacedStates.containsKey(replacedState.getLocation())) return;
         replacedStates.put(replacedState.getLocation(), replacedState);
 
